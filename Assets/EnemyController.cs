3ee786458c5ccbf37029728397d3eeaf07d0c67f
Maximiliano Arias <ariasmaxi89@gmail.com>
2020-06-19 11:11:00 -0300

    feat(enemies): Added enemy behavior

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var objects = GameObject.FindGameObjectsWithTag("Enemy");
+        var objectCount = objects.Length;
+        foreach (var obj in objects)
+        {
+            // Move the players accordingly
+            var rb = obj.GetComponent<Rigidbody2D>();
+            Debug.Log(rb);
+            Vector2 direction = new Vector2(Random.Range(0,10), Random.Range(0,10));
+            rb.velocity = direction * 0.1f;
+        }
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        tillNextSpawn = 0;
+    }
+float tillNextSpawn;
+public GameObject circle;
+    // Update is called once per frame
+   void Update()
+
+{
+    tillNextSpawn += Time.deltaTime;
+    Debug.Log(tillNextSpawn);
+
+    if (tillNextSpawn >= 2)
+    {
+        UnityEngine.Debug.Log("Instantiating circle");
+        Instantiate(circle,  Vector2.zero, Quaternion.identity);
+        tillNextSpawn = 0.0f;
+    }
+} 
+    
+}
