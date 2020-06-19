6d160ef8a463c452f91ba560b0f488f136c7722d
Maximiliano Arias <maximiliano.arias@fidtech.net>
2020-06-19 20:00:30 -0300

    feat(indivEnemies): Added indivdualized enemies scene

---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = new Vector2(Random.Range(0, 10), Random.Range(0, 10));
+        rb.velocity = direction * 0.1f;
+    }
+}
